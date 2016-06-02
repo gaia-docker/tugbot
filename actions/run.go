60e8f205763a59fec8e1fadce5c38d7a9e194b14
Effi <effoeffi@gmail.com>
2016-06-02 19:57:46 +0300

    Project created; docker wrapper & basic cli

---
+++ actions/run.go
@@ -0,0 +1,38 @@
+package actions
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/gaia-docker/tugbot/container"
+)
+
+
+// Run looks at the running Docker containers to see if any of the images
+// used to start those containers is a test container.
+// For each test container it'll create and start a new container according
+// to tugbots' labels
+func Run(client container.Client, names []string) error {
+
+	log.Info("Checking containers for running tests")
+	for _, c := range client.ListContainers(containerFilter(names)) {
+		log.Debug(c)
+	}
+
+	return nil
+}
+
+func allContainersFilter(container.Container) bool { return true }
+
+func containerFilter(names []string) container.Filter {
+	if len(names) == 0 {
+		return allContainersFilter
+	}
+
+	return func(c container.Container) bool {
+		for _, name := range names {
+			if (name == c.Name()) || (name == c.Name()[1:]) {
+				return true
+			}
+		}
+		return false
+	}
+}
