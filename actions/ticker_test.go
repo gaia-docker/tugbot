d31b27be22530fc6ac0e70e2661f0208dec0a9d0
Effi <effoeffi@gmail.com>
2016-12-19 17:40:33 +0200

    testing added for failed to find basic container to start a new container from it;

--- actions/ticker_test.go
+++ actions/ticker_test.go
@@ -145,7 +145,7 @@ func TestRunTickerTestContainers_Iteration2ContainsDifferentListContainers(t *te
 	client.On("ListContainers", mock.AnythingOfType("container.Filter")).
 		Run(func(args mock.Arguments) {
 			touchListContainers = true
-		}).Return([]container.Container{c2}, nil)
+		}).Return([]container.Container{c2}, nil).Once()
 	client.On("Inspect", mock.AnythingOfType("string")).
 		Run(func(args mock.Arguments) {
 			assert.Equal(t, c2.ID(), args.Get(0).(string))
@@ -172,5 +172,48 @@ func TestRunTickerTestContainers_Iteration2ContainsDifferentListContainers(t *te
 
 	assert.True(t, touchListContainers)
 	assert.True(t, touchStartContainerFrom)
+	client.AssertExpectations(t)
+}
+
+func TestRunTickerTestContainers_FailedToFindBasicContainer(t *testing.T) {
+	var wg1, wg2 sync.WaitGroup
+	wg1.Add(1)
+	wg2.Add(1)
+	cc := &dockerclient.ContainerConfig{
+		Labels: map[string]string{
+			container.TugbotTest:       "true",
+			container.TugbotEventTimer: "1ms",
+		},
+	}
+	c := *container.NewContainer(
+		&dockerclient.ContainerInfo{
+			Id:     "cid",
+			Name:   "API tests",
+			Config: cc,
+			State:  stateExited,
+		},
+		nil,
+	)
+
+	client := mockclient.NewMockClient()
+	client.On("ListContainers", mock.AnythingOfType("container.Filter")).
+		Return([]container.Container{c}, nil).Once()
+	// failed to find basic container (to start from it a new container)
+	client.On("Inspect", mock.AnythingOfType("string")).
+		Run(func(args mock.Arguments) {
+			assert.Equal(t, c.ID(), args.Get(0).(string))
+			wg1.Done()
+		}).Return(&container.Container{}, errors.New("Expected :-)")).Once()
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		RunTickerTestContainers(ctx, client, time.Second*10)
+		wg2.Done()
+	}()
+	log.Info("Wating for inpect of ", c.Name())
+	wg1.Wait()
+	cancel()
+	log.Info("Wating for quiting ticker")
+	wg2.Wait()
+
 	client.AssertExpectations(t)
 }
