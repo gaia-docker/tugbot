7409b1827228575e37bf4572ef30f6cb02efe6a6
boriska70@gmail.com <boriska70@gmail.com>
2016-12-21 13:08:48 +0200

    extra logging added temporarily

--- container/container.go
+++ container/container.go
@@ -93,7 +93,9 @@ func (c Container) IsTugbot() bool {
 // it doesn't contain "tugbot.created.from" in the container metadata and it state is "Exited".
 func (c Container) IsTugbotCandidate() bool {
 	ret := false
+	log.Info("Checking container %s for being candidate",c.containerInfo.Name)
 	val, ok := c.containerInfo.Config.Labels[TugbotTest]
+	log.Info("Val is %s", val)
 	if ok && val == "true" {
 		if !c.IsCreatedByTugbot() {
 			ret = c.containerInfo.State.StateString() == "exited"
